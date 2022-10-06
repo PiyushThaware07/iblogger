116eea4c9332c88e049742537a2563ff8ffa3829
piyush <piyushbthaware@gmail.com>
2022-10-07 01:10:17 +0530

    contact

--- websites/urls.py
+++ websites/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.home,name='home'),
     # Url mapping
     path('about',views.about,name='about-index'),
+    path('contact',views.contact,name='contact-index'),
     path('code',views.code,name='code-index'),
 ]
--- websites/views.py
+++ websites/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,HttpResponse
 from blog.models import Category
+from .models import Contact
+# Messages
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -10,5 +13,23 @@ def home(request):
 def about(request):
     return render(request, 'websites/about.html')
 
+def contact(request):
+    cats = Category.objects.all()
+    context = {'cats':cats}
+    
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        msg = request.POST.get('msg')
+
+        if len(name)<2 or len(email)<3:
+            messages.error(request, "Please fill the form correctly")
+
+        else:
+            contact=Contact(name=name, email=email,msg=msg)
+            contact.save()
+            messages.success(request, "Your message has been successfully sent")
+    return render(request, 'websites/contact.html',context)
+
 def code(request):
-    return render(request, 'websites/code.html')
+    return HttpResponse("code")
